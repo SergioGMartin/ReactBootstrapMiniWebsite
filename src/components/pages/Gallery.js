15c0edfa285ec1fb60ebf6934068bafec9ef0303
Sergio-Martin <38584889+SergioGMartin@users.noreply.github.com>
2021-09-30 16:50:22 +0200

    Added pagination to gallery

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,50 +1,79 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 const Gallery = () => {
     /* Logic load API */
 
-    const [gallery,setGallery] = useState([]);
+    const [gallery, setGallery] = useState([]);
+    const [page, setPage] = useState(1);
 
-    const loadGallery = () => {
-        fetch("https://picsum.photos/v2/list?page=6&limit=8")
+    const loadGallery = (page) => {
+        fetch("https://picsum.photos/v2/list?page=" + page + "&limit=6")
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                setGallery(data); })
+                setGallery(data);
+            })
     }
 
-    useEffect(() => { loadGallery(); }, [])
+    const nextPage = (page) => {
+        setPage(page);
+    }
+
+    useEffect(() => { loadGallery(page); }, [page])
 
     const ListImages = () => (
         <div className="row">
-        { gallery.map( (img) => { 
-            const url = "https://picsum.photos/id/" + img.id + "/600/400";
-            return(
-                <div key={img.id} className= "col-12 col-md-4 mb-3" >
-                    <img src={url} className="img-fluid" />
-                    <h4 className="text-center">{ img.author}</h4>
-                </div >
-            )
-        })
-        }
+            {gallery.map((img) => {
+                const url = "https://picsum.photos/id/" + img.id + "/600/400";
+                return (
+                    <div key={img.id} className="col-12 col-md-4 mb-3" >
+                        <a href={img.url} target="_blank" rel="noreferer">
+                            <img src={url} className="img-fluid" />
+                        </a>
+                        <h4 className="text-center">{img.author}</h4>
+                    </div >
+                )
+            })
+            }
         </div>
     )
 
-return (
-    <section>
-        <div className="container-fluid">
-            <div className="row">
-                <h1 className="h3">Gallery</h1>
-            </div>
-            <div className="row">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s..</p>
-                <ListImages />
-            </div>
+    const Pagination = () => (
+        <div className="row mx-auto w-25">
+            <nav aria-label="Page navigation example">
+                <ul className="pagination justify-content-center"> 
+                    <li className="page-item disabled" >
+                        <a className="page-link" onClick={() => page>1 ? nextPage(page - 1) : null} href="#">Previous</a>
+                    </li>
+                    <li className="page-item active"><a className="page-link" onClick={() => nextPage(page)}  href="#">{page}</a></li>
+                    <li className="page-item"><a className="page-link" onClick={() => nextPage(page + 1)}  href="#">{page+1}</a></li>
+                    <li className="page-item"><a className="page-link" onClick={() => nextPage(page + 2)}  href="#">{page+2}</a></li>
+                    <li className="page-item">
+                        <a className="page-link" onClick={() => nextPage(page + 1)} href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    </section>
-)
+    )
+
+    return (
+        <section>
+            <div className="container-fluid">
+                <div className="row">
+                    <h1 className="h3">Gallery</h1>
+                </div>
+                <div className="row">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s..</p>
+                </div>
+                <Pagination />
+                <ListImages />
+                <Pagination />
+            </div>
+
+        </section>
+    )
 }
 
 export default Gallery
