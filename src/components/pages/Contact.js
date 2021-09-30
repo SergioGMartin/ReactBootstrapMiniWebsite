a1c23a721f52dc0fc8a7a7988937f1359dccb88f
Sergio-Martin <38584889+SergioGMartin@users.noreply.github.com>
2021-09-30 14:55:45 +0200

    Added a form and map to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,59 @@
 const Contact = () => (
     <section>
+        
         <div className="container-fluid">
             <div className="row">
-                <h1 className="h3">Contact</h1>
+            <h1 className="h3">Visit Us</h1>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22281.699307929994!2d4.81788463955078!3d45.726834799999985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb00061ad627%3A0x9f3f0744f603f173!2sM2i%20Formation!5e0!3m2!1ses!2sfr!4v1633005836043!5m2!1ses!2sfr" width="600" height="450" allowFullScreen="" loading="lazy"></iframe>
             </div>
             <div className="row">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <h1 className="h3">Contact Us</h1>
             </div>
         </div>
+        <form className="row g-3 m-auto">
+            <div className="col-md-6">
+                <label htmlFor="inputEmail4" className="form-label">Email</label>
+                <input type="email" className="form-control" id="inputEmail4" />
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="inputPassword4" className="form-label">Password</label>
+                <input type="password" className="form-control" id="inputPassword4" />
+            </div>
+            <div className="col-12">
+                <label htmlFor="inputAddress" className="form-label">Address</label>
+                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+            <div className="col-12">
+                <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+                <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="inputCity" className="form-label">City</label>
+                <input type="text" className="form-control" id="inputCity" />
+            </div>
+            <div className="col-md-4">
+                <label htmlFor="inputState" className="form-label">State</label>
+                <select id="inputState" className="form-select">
+                    <option>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div className="col-md-2">
+                <label htmlFor="inputZip" className="form-label">Zip</label>
+                <input type="text" className="form-control" id="inputZip" />
+            </div>
+            <div className="col-12">
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" id="gridCheck" />
+                    <label className="form-check-label" htmlFor="gridCheck">
+                        Check me out
+                    </label>
+                </div>
+            </div>
+            <div className="col-12">
+                <button type="submit" className="btn btn-primary">Send</button>
+            </div>
+        </form>
     </section>
 )
 
